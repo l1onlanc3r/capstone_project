4050f8b55c425e225c94e935c46fc39435b0cd30
Lionlancer <lionlancer@gmail.com>
2023-02-22 18:18:01 +0800

    small update

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 function History({ details, counter }) {
   return (
-    <div className="rounded-lg bg-white text-left shadow-xl transition-all sm:my-3 sm:w-full sm:max-w-lg px-5 py-5 text-sm sm:w-full lg:w-60">
+    <div className="rounded-lg bg-white text-left shadow-xl transition-all sm:my-3 sm:w-full sm:max-w-lg px-5 py-5 text-sm sm:w-full lg:w-70">
       <div className="flex place-content-between">
         <p className="font-medium text-xs">ATTEMPT #{counter}</p>
         <p className="text-xs">
-          Attempt Date: {new Date(details.datetime).toDateString()}
+          Attempt Date:
+          <br />
+          {new Date(details.datetime).toDateString()}
         </p>
       </div>
       <div className="flex place-content-between">
-        <p className="text-2xl my-3 font-medium">
+        <p className="text-2xl my-3 mr-3 font-medium">
           Correct Answers: {details.correct}
         </p>
-        <p className="text-2xl my-3 font-medium">Score: {details.score}</p>
+        <p className="text-2xl my-3 ml-3 font-medium">Score: {details.score}</p>
       </div>
     </div>
   );
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -12,14 +12,24 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <button type="button" className="btn" onClick={navigateToAuth}>
-        Login
-      </button>
-      <button type="button" className="btn" onClick={navigateToRegister}>
-        Register
-      </button>
+    <div className="w-full h-full flex center-items">
+      <div className="m-auto p-10 text-center">
+        <h1 className="font-bold text-4xl">QUIZ APP</h1>
+        <button
+          type="button"
+          className="btn w-full mt-10"
+          onClick={navigateToAuth}
+        >
+          Login
+        </button>
+        <button
+          type="button"
+          className="btn w-full my-5"
+          onClick={navigateToRegister}
+        >
+          Register
+        </button>
+      </div>
     </div>
   );
 }
